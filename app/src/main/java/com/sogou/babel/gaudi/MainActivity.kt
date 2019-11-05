b1635eb5caac19543d68a9614479cf5bb7aae486
赵佳亮 <zhaojialiang@sogou-inc.com>
2019-11-05 21:40:09 +0800

    Add Live Data to ViewModel

--- app/src/main/java/com/sogou/babel/gaudi/MainActivity.kt
+++ app/src/main/java/com/sogou/babel/gaudi/MainActivity.kt
@@ -3,15 +3,24 @@ package com.sogou.babel.gaudi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.sogou.babel.gaudi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val bingVar = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        bingVar.lifecycleOwner = this
         bingVar.firstName = "Albert"
         bingVar.secondName = "Snow"
+        bingVar.viewmodel = viewModel
+
     }
 }
--- app/src/main/java/com/sogou/babel/gaudi/MainViewModel.kt
+++ app/src/main/java/com/sogou/babel/gaudi/MainViewModel.kt
@@ -1,3 +1,37 @@
 package com.sogou.babel.gaudi
 
-class MainViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val _firstName = MutableLiveData("Hello")
+
+    private val _secondName = MutableLiveData("World")
+    private val _likeTimes = MutableLiveData(0)
+
+
+    val firstName: LiveData<String> = _firstName
+    val secondName: LiveData<String> = _secondName
+
+    var likeTimes: LiveData<Int> = _likeTimes
+        private set
+
+    fun onLike() {
+        _likeTimes.value = (_likeTimes.value ?: 0) + 1
+    }
+
+    val popularity: LiveData<Popularity> = Transformations.map(_likeTimes) {
+        when {
+            it > 9 -> Popularity.STAR
+            it > 4 -> Popularity.POPULAR
+            else -> Popularity.NORMAL
+        }
+    }
+
+}
+
+enum class Popularity {
+    STAR, POPULAR, NORMAL
+}
